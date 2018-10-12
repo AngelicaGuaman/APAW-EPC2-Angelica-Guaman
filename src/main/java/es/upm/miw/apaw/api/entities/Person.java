76ad8c5bceeb1c159412926c3efbf406d58040f1
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-10-12 22:45:10 +0200

    added inheritance and deleted duplications code

---
+++ src/main/java/es/upm/miw/apaw/api/entities/Person.java
@@ -0,0 +1,35 @@
+package es.upm.miw.apaw.api.entities;
+
+public class Person {
+    private String id;
+
+    private String nick;
+
+    private String email;
+
+    public Person(String id, String nick) {
+        this.id = id;
+        this.nick = nick;
+        this.email = "";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+}
