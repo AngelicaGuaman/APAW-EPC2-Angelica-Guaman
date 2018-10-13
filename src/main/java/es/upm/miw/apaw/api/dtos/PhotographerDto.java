4acc597a66e717f8a975803aabe9ac651f312024
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-10-13 20:48:03 +0200

    refactoring photograherDto issue#3

--- src/main/java/es/upm/miw/apaw/api/dtos/PhotographerDto.java
+++ src/main/java/es/upm/miw/apaw/api/dtos/PhotographerDto.java
@@ -1,36 +1,16 @@
 package es.upm.miw.apaw.api.dtos;
 
-public class PhotographerDto {
-
-    private String nick;
-
-    private String email;
+public class PhotographerDto extends PersonDto {
 
     public PhotographerDto(String nick) {
-        this.nick = nick;
-    }
-
-    public String getNick() {
-        return nick;
-    }
-
-    public void setNick(String nick) {
-        this.nick = nick;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+        super(nick);
     }
 
     @Override
     public String toString() {
         return "PhotographerDto{" +
-                "nick='" + nick + '\'' +
-                ", email='" + email + '\'' +
+                "nick='" + this.getNick() + '\'' +
+                ", email='" + this.getEmail() + '\'' +
                 '}';
     }
 }
